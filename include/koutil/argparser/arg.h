4046372b3d3e561e1a3d0a62bb2be9300da8e385
Lukáš Koliandr <koliandr.lu@gmail.com>
2024-06-01 19:06:36 +0200

    feat: argument parser with subcommands

--- include/koutil/argparser/arg.h
+++ include/koutil/argparser/arg.h
@@ -1,26 +1,31 @@
 #ifndef KOUTIL_ARGPARSER_ARG_H
 #define KOUTIL_ARGPARSER_ARG_H
 
-#include <algorithm>
 #include <array>
 #include <cassert>
-#include <concepts>
 #include <cstddef>
-#include <iterator>
-#include <optional>
 #include <ranges>
 #include <span>
 #include <string_view>
 #include <type_traits>
 #include <utility>
 
-namespace koutil::experimental::argparser {
+namespace koutil::argparser {
 
 struct Arg;
 
+/**
+ * @brief Checks if a type is Arg.
+ *
+ * @tparam T The type to check.
+ * @return True if T is Arg, false otherwise.
+ */
 template <typename T>
-concept arg_type = std::is_same_v<Arg, std::decay_t<T>>;
+concept is_arg = std::is_same_v<Arg, std::decay_t<T>>;
 
+/**
+ * @brief Base structure representing a subcommand argument.
+ */
 struct Arg {
     enum Type {
         OPTION_FLAG,
@@ -31,21 +36,50 @@ struct Arg {
     const std::string_view long_name;
     const std::string_view description;
 
+    /**
+     * @brief Constructs an argument of type OPTION_FLAG with a short name and description.
+     *
+     * @param opt The short name of the argument.
+     * @param desc The description of the argument.
+     * @return A constructed argument.
+     */
     static constexpr Arg option_flag(char opt, std::string_view desc) {
         assert(opt != '\0');
         return { OPTION_FLAG, opt, "", desc };
     }
 
+    /**
+     * @brief Constructs an argument of type OPTION_FLAG with a long name and description.
+     *
+     * @param long_name The long name of the argument.
+     * @param desc The description of the argument.
+     * @return A constructed argument.
+     */
     static constexpr Arg option_flag(std::string_view long_name, std::string_view desc) {
         assert(!long_name.empty());
         return { OPTION_FLAG, '\0', long_name, desc };
     }
 
+    /**
+     * @brief Constructs an argument of type OPTION_FLAG with both short and long names and a description.
+     *
+     * @param opt The short name of the argument.
+     * @param long_name The long name of the argument.
+     * @param desc The description of the argument.
+     * @return A constructed argument.
+     */
     static constexpr Arg option_flag(char opt, std::string_view long_name, std::string_view desc) {
         assert(opt != '\0' && !long_name.empty());
         return { OPTION_FLAG, opt, long_name, desc };
     }
 
+    /**
+     * @brief Constructs an argument of type OPTION_VALUE with a long name and description.
+     *
+     * @param long_name The long name of the argument.
+     * @param desc The description of the argument.
+     * @return A constructed argument.
+     */
     static constexpr Arg option_value(std::string_view long_name, std::string_view desc) {
         assert(!long_name.empty());
         return { OPTION_VALUE, '\0', long_name, desc };
@@ -77,26 +111,28 @@ private:
 
 class ArgumentsBase {
 public:
-    constexpr virtual ~ArgumentsBase()                                                               = default;
-    [[nodiscard]] constexpr virtual std::size_t size() const                                         = 0;
-    [[nodiscard]] constexpr virtual std::size_t count_short_args() const                             = 0;
-    [[nodiscard]] constexpr virtual std::size_t count_long_args() const                              = 0;
-    [[nodiscard]] constexpr virtual std::optional<const Arg*> find_short(char name) const            = 0;
-    [[nodiscard]] constexpr virtual std::optional<const Arg*> find_long(std::string_view name) const = 0;
-    [[nodiscard]] constexpr virtual std::span<const Arg> args() const                                = 0;
+    constexpr virtual ~ArgumentsBase()                                                = default;
+    [[nodiscard]] constexpr virtual std::size_t size() const                          = 0;
+    [[nodiscard]] constexpr virtual std::size_t count_short_args() const              = 0;
+    [[nodiscard]] constexpr virtual std::size_t count_long_args() const               = 0;
+    [[nodiscard]] constexpr virtual const Arg* find_short(char name) const            = 0;
+    [[nodiscard]] constexpr virtual const Arg* find_long(std::string_view name) const = 0;
+    [[nodiscard]] constexpr virtual std::span<const Arg> args() const                 = 0;
 };
 
+/**
+ * @brief Checks if a type is derived from ArgumentsBase.
+ *
+ * @tparam T The type to check.
+ * @return True if T is derived from ArgumentsBase, false otherwise.
+ */
 template <typename T>
-concept arguments = requires(T t, char c, std::string_view sv) {
-    std::is_base_of_v<ArgumentsBase, T>;
-    { t.size() } -> std::same_as<std::size_t>;
-    { t.count_short_args() } -> std::same_as<std::size_t>;
-    { t.count_long_args() } -> std::same_as<std::size_t>;
-    { t.find_short(c) } -> std::same_as<std::optional<const Arg*>>;
-    { t.find_long(sv) } -> std::same_as<std::optional<const Arg*>>;
-};
+concept are_arguments = std::is_base_of_v<ArgumentsBase, T>;
 
-template <arg_type... Args> class Arguments : public ArgumentsBase {
+/**
+ * @brief Base class for argument collections.
+ */
+template <is_arg... Args> class Arguments : public ArgumentsBase {
 private:
     using args_t     = std::array<Arg, sizeof...(Args)>;
     using args_ref_t = std::pair<std::size_t, std::array<std::size_t, sizeof...(Args)>>;
@@ -112,36 +148,68 @@ public:
 
     ~Arguments() override = default;
 
+    /**
+     * @brief Gets the number of arguments.
+     *
+     * @return The number of arguments.
+     */
     [[nodiscard]] constexpr std::size_t size() const override { return sizeof...(Args); }
 
+    /**
+     * @brief Gets the number of short arguments.
+     *
+     * @return The number of short arguments.
+     */
     [[nodiscard]] constexpr std::size_t count_short_args() const override { return m_short_args.first; }
 
+    /**
+     * @brief Gets the number of long arguments.
+     *
+     * @return The number of long arguments.
+     */
     [[nodiscard]] constexpr std::size_t count_long_args() const override { return m_long_args.first; }
 
-    [[nodiscard]] constexpr std::optional<const Arg*> find_short(char name) const override {
+    /**
+     * @brief Finds an argument by its short name.
+     *
+     * @param name The short name of the argument to find.
+     * @return A pointer to the argument if found, nullptr otherwise.
+     */
+    [[nodiscard]] constexpr const Arg* find_short(char name) const override {
 
         std::span view(m_short_args.second.begin(), m_short_args.first);
         for (auto i : view) {
             if (m_args[i].short_name == name) {
-                return std::make_optional(&m_args[i]);
+                return &m_args[i];
             }
         }
 
-        return {};
+        return nullptr;
     }
 
-    [[nodiscard]] constexpr std::optional<const Arg*> find_long(std::string_view name) const override {
+    /**
+     * @brief Finds an argument by its long name.
+     *
+     * @param name The long name of the argument to find.
+     * @return A pointer to the argument if found, nullptr otherwise.
+     */
+    [[nodiscard]] constexpr const Arg* find_long(std::string_view name) const override {
 
         std::span view(m_long_args.second.begin(), m_long_args.first);
         for (auto i : view) {
             if (m_args[i].long_name == name) {
-                return std::make_optional(&m_args[i]);
+                return &m_args[i];
             }
         }
 
-        return {};
+        return nullptr;
     }
 
+    /**
+     * @brief Gets a span of all arguments.
+     *
+     * @return A span containing all arguments.
+     */
     [[nodiscard]] constexpr std::span<const Arg> args() const override { return m_args; }
 
 private:
@@ -162,6 +230,14 @@ private:
         }
     }
 
+    /**
+     * @brief Checks if there are duplicate arguments.
+     *
+     * @tparam Proj The type of the member projection.
+     * @param search The reference to search in.
+     * @param member The member to project.
+     * @return True if duplicates are found, false otherwise.
+     */
     template <typename Proj> constexpr bool contains_duplicates(const args_ref_t& search, Proj member) {
         std::span view(search.second.begin(), search.first);
 
